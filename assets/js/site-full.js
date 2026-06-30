7483d4a0c1a28504a4930bb5f0abb43f28eb4f46
Jappi-Boss <ababemobile@gmail.com>
2026-06-30 08:49:25 +0300

    Add full-site navigation script

---
+++ assets/js/site-full.js
@@ -0,0 +1,1 @@
+const btn=document.querySelector('.menu-btn');const nav=document.querySelector('.nav-links');if(btn&&nav){btn.addEventListener('click',()=>nav.classList.toggle('open'))}document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener('click',()=>nav?.classList.remove('open')));
