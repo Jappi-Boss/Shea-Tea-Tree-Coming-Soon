06e3b7b063e885647a697335abeab31e79da3109
Jappi-Boss <ababemobile@gmail.com>
2026-07-09 00:24:09 +0300

    Publish reviewed Coconut Extract Shampoo detail content

--- assets/js/product-image-loader.js
+++ assets/js/product-image-loader.js
@@ -1,1 +1,16 @@
-(()=>{if(document.querySelector('script[data-stt-new-products]'))return;const script=document.createElement('script');script.src='assets/js/new-products.js?v=20260708-1';script.defer=true;script.dataset.sttNewProducts='true';document.head.appendChild(script);})();
+(() => {
+  if (!document.querySelector('script[data-stt-new-products]')) {
+    const script = document.createElement('script');
+    script.src = 'assets/js/new-products.js?v=20260709-1';
+    script.defer = true;
+    script.dataset.sttNewProducts = 'true';
+    document.head.appendChild(script);
+  }
+  if (!document.querySelector('script[data-stt-coconut-content]')) {
+    const script = document.createElement('script');
+    script.src = 'assets/js/coconut-product-content.js?v=20260709-1';
+    script.defer = true;
+    script.dataset.sttCoconutContent = 'true';
+    document.head.appendChild(script);
+  }
+})();
